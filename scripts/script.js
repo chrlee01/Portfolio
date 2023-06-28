64a436317e9d9cb2f6e4a58c8185937f9f5f4ba5
chrlee01 <chrlee21@gmail.com>
2023-06-28 14:02:23 -0600

    changed text form to white

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll('.nav-link');
-
-        navLinks.forEach(link => {
-          link.addEventListener('click', function() {
-            navLinks.forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-          });
-        });
